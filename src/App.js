8a9b39aa51861a2ec28c2bb3f91a8f599b78ad6c
Nils Kriedner <nkriedner@googlemail.com>
2021-09-17 16:33:47 +0200

    Create functional PhotoContainer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav";
 import SearchForm from "./components/SearchForm";
+import PhotoContainer from "./components/PhotoContainer";
 
 function App() {
     return (
         <div className="container">
             <SearchForm />
             <Nav />
+            <PhotoContainer />
         </div>
     );
 }
