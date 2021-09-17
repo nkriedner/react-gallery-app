d9b18b2ac60be47a4f6bd3b230266fc3f7022efb
Nils Kriedner <nkriedner@googlemail.com>
2021-09-17 17:57:20 +0200

    Create Flickr apikey and import it from config.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Nav from "./components/Nav";
 import SearchForm from "./components/SearchForm";
 import PhotoContainer from "./components/PhotoContainer";
+import apiKey from "./config";
 
 function App() {
+    // console.log(apiKey);
+
     const photos = [
         {
             src: "https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg",
