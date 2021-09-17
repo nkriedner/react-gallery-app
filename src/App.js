2f2263a6a45911fab0609b3b44d7cf15bad02ccf
Nils Kriedner <nkriedner@googlemail.com>
2021-09-17 17:16:36 +0200

    Create placeholder photo data list and use map to display its photos

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import SearchForm from "./components/SearchForm";
 import PhotoContainer from "./components/PhotoContainer";
 
 function App() {
+    const photos = [
+        {
+            src: "https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg",
+        },
+        {
+            src: "https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg",
+        },
+        {
+            src: "https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg",
+        },
+        {
+            src: "https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg",
+        },
+    ];
+
     return (
         <div className="container">
             <SearchForm />
             <Nav />
-            <PhotoContainer />
+            <PhotoContainer photos={photos} />
         </div>
     );
 }
