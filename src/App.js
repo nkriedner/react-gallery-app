f5c0aa641f6742b99d46f804cfb467d95f56cec8
Nils Kriedner <nkriedner@googlemail.com>
2021-09-27 10:09:50 +0200

    Change PhotoContainer into class component + delete some comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import Nav from "./components/Nav";
 import SearchForm from "./components/SearchForm";
@@ -6,65 +6,32 @@ import PhotoContainer from "./components/PhotoContainer";
 import apiKey from "./config";
 import React from "react";
 
-const photos = [
-    {
-        id: 1,
-        src: "https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg",
-    },
-    {
-        src: "https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg",
-        id: 2,
-    },
-    {
-        src: "https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg",
-        id: 3,
-    },
-    {
-        src: "https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg",
-        id: 4,
-    },
-];
 export default class App extends React.Component {
     constructor() {
         super();
         this.state = {
             images: [],
-            fetchedData: false,
         };
     }
 
     componentDidMount() {
-        // Flickr Endpoint: https:/api.flickr.com/services
         const key = apiKey.key;
-        // console.log("key:", key);
-        // console.log(typeof key);
 
         const data = {
             method: "flickr.photos.search",
             api_key: key,
-            text: "cat", // search text
-            // format: "json",
-            tags: "cat",
+            text: "yoga", // search text
+            tags: "yoga",
             per_page: 12,
-            // sort: "interestingness-desc",
-            // license: "4"
         };
         const parameters = new URLSearchParams(data);
-        // console.log("parameters:", parameters);
         const url = `https://api.flickr.com/services/rest/?${parameters}&format=json&nojsoncallback=1`;
-        // const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api`
-        // console.log("url:", url);
 
         axios
-            .get(
-                // "https://www.flickr.com//services/rest/?method=flickr.test.echo&name=value"
-                // `https://www.flickr.com//services/rest/?method=flickr.photos.search&api_key=${key}`
-                url
-            )
+            .get(url)
             .then((response) => {
                 this.setState({
                     images: response.data.photos.photo,
-                    fetchedData: true,
                 });
                 console.log("images:", response.data.photos.photo);
             })
@@ -79,50 +46,9 @@ export default class App extends React.Component {
                 <div className="container">
                     <SearchForm />
                     <Nav />
-                    <PhotoContainer
-                        photos={photos}
-                        images={this.state.images}
-                    />
+                    <PhotoContainer images={this.state.images} />
                 </div>
             </BrowserRouter>
         );
     }
 }
-
-/*
-function App() {
-    // console.log(apiKey);
-    
-
-    const photos = [
-        {
-            id: 1,
-            src: "https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg",
-        },
-        {
-            src: "https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg",
-            id: 2,
-        },
-        {
-            src: "https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg",
-            id: 3,
-        },
-        {
-            src: "https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg",
-            id: 4,
-        },
-    ];
-
-    return (
-        <BrowserRouter>
-            <div className="container">
-                <SearchForm />
-                <Nav />
-                <PhotoContainer photos={photos} />
-            </div>
-        </BrowserRouter>
-    );
-}
-
-export default App;
-*/
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -2,12 +2,8 @@ import React from "react";
 import NotFound from "./NotFound";
 import Photo from "./Photo";
 
-class PhotoContainer extends React.Component {
+export default class PhotoContainer extends React.Component {
     render() {
-        // const imagesList = this.props.data;
-        // if (imagesList.length > 0) {
-        //     console.log("imagesList:", imagesList);
-        // }
         return (
             <div className="photo-container">
                 <h2>Results</h2>
@@ -15,19 +11,15 @@ class PhotoContainer extends React.Component {
                     {this.props.images.map((photo) => {
                         return (
                             <Photo
-                                // url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`}
                                 url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`}
                                 key={photo.id}
                             />
                         );
                     })}
 
-                    {/* <!-- Not Found --> */}
                     <NotFound />
                 </ul>
             </div>
         );
     }
 }
-
-export default PhotoContainer;
